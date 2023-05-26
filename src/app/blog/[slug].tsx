6e5fb011902f16cb9ab29c7fc178fd0115ea87af
mikechess <mikecook450@gmail.com>
2023-05-26 16:28:35 -0400

    Routing in Next.js. Pages Folder

---
+++ src/app/blog/[slug].tsx
@@ -0,0 +1,3 @@
+const BlogItem = () => <h1 className="text-white text-3xl">BlogItem Page</h1>
+
+export default BlogItem
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+const Blog = () => <h1 className="text-white text-3xl">Blog Page</h1>
+
+export default Blog
+
+
+
+
+
